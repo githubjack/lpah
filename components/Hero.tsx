de61593ca7c7a924fc59413fc5d5661b83fa11f3
githubjack <jack.ui.maynard@gmail.com>
2022-05-27 11:53:18 +0100

    hero section development

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,20 @@
 import * as React  from 'react';
-import { Link, useLocation, Outlet } from 'react-router-dom';
 import './hero-grid.css';
 
 
 export function Hero() {
   return (
     <div className="hero-grid">
-      <h2>Our Company History</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer egestas sagittis consectetur.</p>
-      <button className="button hero-cta">Let's get started</button>
+      <div><h2>Our Company History</h2></div>
+    <div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer egestas sagittis consectetur.</p>
+    </div>
+      {/**
+       <div>
+        <a href="#" className="hero-cta">Let's get started</a>
+      </div>
+       * 
+       */}
     </div>
   );
 }
